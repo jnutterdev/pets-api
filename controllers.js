2d46ae7ed5112626d4f572ee38034e61f21cbf12
terribledevice <longlost@gmail.com>
2020-11-24 18:30:52 -0500

    Started fleshing out owners controllers

--- controllers.js
+++ controllers.js
@@ -26,22 +26,77 @@ var owners = [{
 
 var primaryId = 1;
 
+// GET /api/owners
 exports.findAll = (req, res) => {
-    return;
+    if (!owners.length) {
+        next();
+    }
+    res.send(owners);
 };
 
+// GET /api/owners/:id
 exports.findOne = (req, res) => {
-    return;
+    const id = req.params.id;
+    let owner = owners.find((o) => {
+        return o.id === Number(id)
+    });
+
+    const filter = req.body.filter || [];
+    if (filter.length) {
+        let newOwner = {};
+        filter.forEach((f) => {
+            newOwner[f] = owner[f];
+        })
+        res.status(200).send(newOwner);
+    }
+    res.status(200).send(owner);
 };
 
+// POST /api/owners
 exports.create = (req, res) => {
-    return;
+    owners.push({
+        id: primaryId,
+        name: req.body.name,
+        pets: req.body.pets
+    });
+    primaryId++;
+
+    res.status(200).json({
+        message: "Owner and pet created succesfully"
+    })
 };
 
+// PUT /api/owners/:id
 exports.update = (req, res) => {
     return;
 };
 
+// DELETE /api/owners/:id
 exports.delete = (req, res) => {
     return;
 };
+
+// GET /api/owners/:id/pets
+exports.allPets = (req, res) => {
+    return;
+}
+
+// GET /api/owners/:id/pets/:petId
+exports.onePet = (req, res) => {
+    return;
+}
+
+// POST /api/owners/:id/pets
+exports.newPet = (req, res) => {
+    return;
+}
+
+// PUT /api/owners/:id/pets/:petId
+exports.updatePet = (req, res) => {
+    return;
+}
+
+// DELETE /api/owners/:id/pets/:petId
+exports.deletePet = (req, res) => {
+    return;
+}
--- routes.js
+++ routes.js
@@ -24,16 +24,24 @@ router.put('/:id', controller.update);
 
 router.delete('/:id', controller.delete);
 
-
 // GET /api/owners/:id/pets
+
+router.get('/:id/pets', controller.allPets);
 
 // GET /api/owners/:id/pets/:petId
 
+router.get('/:id/pets/:petId', controller.onePet);
+
 // POST /api/owners/:id/pets
+
+router.post('/:id/pets', controller.newPet);
 
 // PUT /api/owners/:id/pets/:petId
 
+router.put('/:id/pets/:petId', controller.updatePet);
+
 // DELETE /api/owners/:id/pets/:petId
 
+router.delete('/:id/pets/:petId', controller.deletePet);
 
 module.exports = router;
